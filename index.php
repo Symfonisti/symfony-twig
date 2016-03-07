48ac96647ea273dac91afca3459229885ba5a145
Rostislav Vitek <vitek@netdevelo.cz>
2016-03-07 22:07:07 +0100

    array items and object properties access

--- index.php
+++ index.php
@@ -2,7 +2,37 @@
 
 //index.php
 require_once __DIR__ . '/vendor/autoload.php';
+
+class Movie {
+
+	/**
+	 * @var string
+	 */
+	private $title;
+
+	/**
+	 * @param string $title
+	 */
+	public function __construct($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+}
+
 $loader = new Twig_Loader_Filesystem('Resources/views');
 $twig = new Twig_Environment($loader);
-echo $twig->render('index.html.twig', ['name' => 'world']);
-// Hello world!
+$movieAsObject = new Movie('Kill Bill');
+$movieAsArray = ['title' => 'Matrix'];
+echo $twig->render('index.html.twig', [
+	'movie1' => $movieAsObject,
+	'movie2' => $movieAsArray,
+]);
+// My favourite movie: Kill Bill
+// My friend`s favourite movie: Matrix
